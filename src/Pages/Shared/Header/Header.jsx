5c3236c22f326c8d1ad067d454983512efc0ae16
Chowon-Hasan <chowonhasan7@gmail.com>
2023-05-02 17:57:32 +0600

    complete header

--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,11 +1,39 @@
 import React from "react";
 import "./Header.css";
+import { Button, Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div>
-      <h2>this is header</h2>
-    </div>
+    <section className="Head_bg">
+      <Container>
+        <Navbar expand="lg">
+          <Container fluid>
+            <Navbar.Brand href="/" className="Logo">
+              Italian's Chefs
+            </Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+              <Nav
+                className="ms-auto my-2 my-lg-0"
+                style={{ maxHeight: "100px; align-items: center;" }}
+                navbarScroll
+              >
+                <Link className="nav_link" to="/">
+                  Home
+                </Link>
+                <Link className="nav_link">Blog</Link>
+                <Link className="nav_link">About</Link>
+                <Link>
+                  <Button className="log_btn">Log in</Button>
+                </Link>
+                <img src="" alt="" />
+              </Nav>
+            </Navbar.Collapse>
+          </Container>
+        </Navbar>
+      </Container>
+    </section>
   );
 };
 
