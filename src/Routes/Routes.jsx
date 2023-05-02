0b30e2fd61689ecdf7c53840c3bb4db1e463efff
Chowon-Hasan <chowonhasan7@gmail.com>
2023-05-02 16:51:56 +0600

    set up layout components

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,18 @@
+import { Router, createBrowserRouter } from "react-router-dom";
+import MainLayout from "../Layout/MainLayout";
+import Home from "../Pages/main Pages/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+    ],
+  },
+]);
+
+export default router;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCWioPaZSr7kOSUBPN7GVbLohoxKxjFHUo",
+  authDomain: "chefs-hunter-a2d03.firebaseapp.com",
+  projectId: "chefs-hunter-a2d03",
+  storageBucket: "chefs-hunter-a2d03.appspot.com",
+  messagingSenderId: "886681149108",
+  appId: "1:886681149108:web:61754fc5e15f23c95b81e1",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
