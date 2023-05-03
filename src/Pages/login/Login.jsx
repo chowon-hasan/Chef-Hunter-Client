a72814edc5cba2af31f2f6785d1da87c63ad3df3
Chowon-Hasan <chowonhasan7@gmail.com>
2023-05-04 03:14:36 +0600

    completed the log pages and router set up

--- src/Pages/login/Login.jsx
+++ src/Pages/login/Login.jsx
@@ -2,9 +2,16 @@ import React, { useContext } from "react";
 import { Button, Col, Container, Form } from "react-bootstrap";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
+import app from "../../firebase/firebase.config";
+import { getAuth, signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider, GithubAuthProvider } from "firebase/auth";
+
+const auth = getAuth(app);
 
 const Login = () => {
   const { signInUser } = useContext(AuthContext);
+  const provider = new GoogleAuthProvider();
+  const providerGit = new GithubAuthProvider();
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
@@ -27,10 +34,31 @@ const Login = () => {
       });
     console.log(email, password);
   };
+  const handlePopup = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const logedUser = result.user;
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleGithub = () => {
+    signInWithPopup(auth, providerGit)
+      .then((result) => {
+        const logedInUser = result.user;
+        navigate(from, { replace: true });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   return (
     <div className="my-5">
       <Container>
-        <Col className="m-auto" lg={6}>
+        <Col className="m-auto border p-5" lg={6}>
           <Form onSubmit={handleLogin}>
             <Form.Group className="mb-3" controlId="formBasicEmail">
               <Form.Label>Email address</Form.Label>
@@ -59,6 +87,11 @@ const Login = () => {
               Already have an account? <Link to="/register">Register here</Link>
             </Form.Text>
           </Form>
+          <div className="">
+            <h2>Sign in with goole</h2>
+            <Button onClick={handlePopup}>Google</Button>
+            <Button onClick={handleGithub}>Github</Button>
+          </div>
         </Col>
       </Container>
     </div>
--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -21,6 +21,7 @@ const AuthProvider = ({ children }) => {
     setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
+
   const signInUser = (email, password) => {
     setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
