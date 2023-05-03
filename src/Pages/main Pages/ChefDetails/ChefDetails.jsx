c821b783c714cf24a630a7ef985d30e83b0bcacb
Chowon-Hasan <chowonhasan7@gmail.com>
2023-05-03 18:34:08 +0600

    added chefDetails banner and api with data

--- src/Pages/main Pages/ChefDetails/ChefDetails.jsx
+++ src/Pages/main Pages/ChefDetails/ChefDetails.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
+import "./ChefFetails.css";
 import { useLoaderData, useParams } from "react-router-dom";
+import { Col, Container, Row } from "react-bootstrap";
 
 const ChefDetails = () => {
   const [datastate, setDataState] = useState({});
@@ -13,17 +15,45 @@ const ChefDetails = () => {
 
   const {
     id,
+    name,
     recipeName,
     image,
     ingredients,
     cookingMethod,
     rating,
     recipe_image,
+    yearsOfExperience,
+    numberOfRecipes,
+    description,
+    likes,
   } = datastate;
 
   console.log(datastate);
 
-  return <div></div>;
+  return (
+    <section className="details_bg">
+      <Container>
+        <Row>
+          <Col className="d-flex align-items-center" lg={6}>
+            <div className="details_content">
+              <h1>{name}</h1>
+              <p>{description}</p>
+              <div className="main_details">
+                <span>Number Of Recipes {numberOfRecipes}</span>
+                <span>Likes {likes}</span>
+                <span>Experience {yearsOfExperience} Years</span>
+              </div>
+            </div>
+          </Col>
+          <Col lg={6}>
+            <div className="text-center">
+              <img className="chef_img" src={image} alt="" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 };
 
 export default ChefDetails;
